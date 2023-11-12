53d6117948832eedc9d556f3caaf0f06e533a31f
nihalkarim <nihalkarim@gmail.com>
2023-11-11 19:22:56 -0500

    domo b

---
+++ server/models/Domo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        set: setName,
+    },
+    age: {
+        type: Number,
+        miin: 0,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: 'Account',
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+    name: doc.name,
+    age: doc.age,
+});
+
+
+DomoModel = mongoose.model('Domo', DomoSchema);
+module.exports = DomoModel;
